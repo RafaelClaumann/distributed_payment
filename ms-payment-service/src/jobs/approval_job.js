b6145003d4c55338c0bffaa831bf1b296c99499c
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2026-04-25 09:05:51 -0300

    docs: add comment explaining async approval simulation in scheduleApproval

--- ms-payment-service/src/jobs/approval_job.js
+++ ms-payment-service/src/jobs/approval_job.js
@@ -1,6 +1,9 @@
 const { updateTransactionStatus } = require("../lib/transaction.js");
 const transactionStatus = require("../enums/transaction_status.js");
 
+// Simula o processamento assíncrono de aprovação de uma transação.
+// Após um delay aleatório entre 1s e 10s, atualiza o status para SUCCESS no banco.
+// Chamado de forma não bloqueante logo após persistir a transação em PaymentService.
 function scheduleApproval(transaction) {
   const delay = Math.random() * 9000 + 1000; // 1s ~ 10s
 
